57b173a3c15f97cdfdf6e23e71d711cad348c347
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-18 18:06:21 +0800

    copy

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,10 +1,21 @@
-score = float(input("Enter score: "))
-if score < 0 or score >100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
+min_score = 0
+max_score = 100
+import random
+
+def generate_random_score(min_score, max_score):
+    return random.randint(min_score, max_score)
+random_score = generate_random_score(min_score, max_score)
+
+
+def main():
+    score = float(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
     else:
-     print("Bad")
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        else:
+            return "Bad"
+
