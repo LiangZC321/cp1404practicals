1da1d954525edfb96696f2e0cdb9338e2066392f
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-19 19:50:10 +0800

    done

---
+++ prac_02/score_menu.py
@@ -0,0 +1,63 @@
+"""
+display menu
+get choice
+while choice != <quit option>
+    if choice == <first option>
+        <do first task>
+    else if choice == <second option>
+        <do second task>
+    ...
+    else if choice == <n-th option>
+        <do n-th task>
+    else
+        display invalid input error message
+    display menu
+    get choice
+<do final thing, if needed>
+"""
+min_score = 0
+max_score = 100
+excellent_score = 90
+passable_score = 50
+
+def main():
+    import random
+
+    score = random.randint(0, 100)
+    display_menu()
+    choice = input("Enter your choice: ").lower()
+    while choice != "q":
+        if choice == "g":
+            score = float(input("Enter score:(0-100)"))
+            return score
+        elif choice == "p":
+            result = evaluate_score(score)
+        elif choice == "s":
+            print("*" * score)
+        else:
+            print("invalid choice")
+            display_menu()
+            choice = input("Enter your choice: ").lower()
+    print("farewell")
+
+def display_menu():
+    print("(G)et a valid score")
+    print("(S)how stars")
+    print("(Q)uit")
+
+def evaluate_score(score):
+    if score < min_score or score > max_score:
+        result = "Invalid score"
+    else:
+        if score >= excellent_score:
+            result = "Excellent"
+        elif score >= passable_score:
+            result = "Passable"
+        else:
+            result = "Bad"
+        return result
+
+
+
+
+
