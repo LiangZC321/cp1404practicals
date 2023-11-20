4af50ac8f5be9e7f9f9e70e59b7e226e73a2101a
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-20 16:15:15 +0800

    fixed a lot

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,4 +6,18 @@ name_file.close()
 name_file = open("name.txt", "r")
 user_name = name_file.read()
 print(f"Your name is {user_name}")
-name_file.close()
+
+
+number_file = open("numbers.txt", "r")
+
+num1 = int(number_file.readline())
+num2 = int(number_file.readline())
+sum_numbers = num1 + num2
+print(f"Sum of the first two numbers: {sum_numbers}")
+number_file.close()
+
+number_file = open("numbers.txt", "r")
+all_numbers = [int(line) for line in number_file.readlines()]
+total_of_all = sum(all_numbers)
+print(f"Total for all numbers: {total_of_all}")
+
