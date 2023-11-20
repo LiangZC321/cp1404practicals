586cce12aaf22ce55beb88de585494ae7b357b16
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-20 13:39:23 +0800

    done

---
+++ prac_03/randoms.py
@@ -0,0 +1,11 @@
+"""
+in line1 I saw 7. I can see smallest as 5 largest as 20
+
+In line2 I saw 3, smallest: 3, largest: 9. cannot produce 4.
+
+In line3 I saw 3.8022044283349756 smallest:2.5 largest:5.5
+
+"""
+import random
+random_number = random.randint(1, 100)
+print(random_number)
