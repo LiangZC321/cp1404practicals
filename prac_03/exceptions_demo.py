fd9065897b8964ff5330a209f86de1ce8c62b24e
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-20 14:55:19 +0800

    avoid the zero

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,10 +9,17 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
+    if denominator == 0:
+        print("Cannot divide by zero!")
+    else:
+     fraction = numerator / denominator
+     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
+
+"""
+answer:
+1.when the user enters a value that cannot be converted to an integer 
+2.enters a denominator of 0
+"""
