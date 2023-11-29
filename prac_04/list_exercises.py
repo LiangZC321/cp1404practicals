123bb35cadd86925fe2d9779ffa49c50d60eec45
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-29 19:03:19 +0800

    done

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,10 @@ print("The last number is", numbers[-1])
 print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The average of the numbers is", sum(numbers) / len(numbers))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+user_name = input("ur name:")
+if user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
