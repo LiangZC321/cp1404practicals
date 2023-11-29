06956ebebccd8df6d160e322e4e27eb7f1fccfcc
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-11-29 19:57:38 +0800

    done

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,20 @@
+import random
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+NUM_QUICK_PICKS = 6
+
+def generate_quick_pick():
+    return sorted(random.sample(range(MIN_NUMBER, MAX_NUMBER + 1), NUM_QUICK_PICKS))
+
+def main():
+    # user part
+    num_quick_picks = int(input("How many quick picks? "))
+
+    for i in range(1, num_quick_picks + 1):
+        quick_pick = generate_quick_pick()
+        print(f"{i:2}", end=" ")
+        for number in quick_pick:
+            print(f"{number:2}", end=" ")
+        print()
+
+main()
