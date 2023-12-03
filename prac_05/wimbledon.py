88e586db5b09f6acecd9314bc1bbff01b4b5fd92
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 02:33:48 +0800

    with functions

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,25 +1,46 @@
 import csv
 from collections import defaultdict
 
-filename = "wimbledon.csv"
+def read_csv(filename):
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.DictReader(in_file)
+        data = list(reader)
+    return data
 
-champion_wins = defaultdict(int)
-countries_set = set()
-
-with open(filename, "r", encoding="utf-8-sig") as in_file:
-    reader = csv.DictReader(in_file)
-
-    for row in reader:
+def process_champions(data):
+    champion_wins = defaultdict(int)
+    for row in data:
         champion = row["Champion"]
-        country = row["Country"]
         champion_wins[champion] += 1
+    return champion_wins
+
+def process_countries(data):
+    countries_set = set()
+    for row in data:
+        country = row["Country"]
         countries_set.add(country)
+    return countries_set
 
-print("Wimbledon Champions:")
+def display_champions(champion_wins):
+    print("Wimbledon Champions:")
+    for champion, wins in sorted(champion_wins.items()):
+        print(f"{champion} {wins}")
 
-for champion, wins in sorted(champion_wins.items()):
-    print(f"{champion} {wins}")
+def display_countries(countries_set):
+    sorted_countries = sorted(countries_set)
+    countries_string = ", ".join(sorted_countries)
+    print("\nThese countries have won Wimbledon:")
+    print(countries_string)
 
-sorted_countries = sorted(countries_set)
-print("\nThese countries have won Wimbledon:")
-print(", ".join(sorted_countries))
+def main():
+    filename = "wimbledon.csv"
+    data = read_csv(filename)
+
+    champion_wins = process_champions(data)
+    display_champions(champion_wins)
+
+    countries_set = process_countries(data)
+    display_countries(countries_set)
+
+
+main()
