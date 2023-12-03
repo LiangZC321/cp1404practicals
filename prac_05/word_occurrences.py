50cc877398bd6c4fde5d419290ef0bcca307b94d
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 01:00:28 +0800

    done, working well

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,2 +1,11 @@
 word_input = input("Enter a string: ")
 words = word_input.split()
+word_counts = {}
+
+for word in words:
+    word = word.lower()
+    word_counts[word] = word_counts.get(word, 0) + 1
+
+print("Word Counts:")
+for word, count in word_counts.items():
+    print(f"{word} : {count}")
