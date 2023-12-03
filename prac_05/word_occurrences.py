c8b8379462dfef8544d8855a233821d4a6495258
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 01:22:01 +0800

    align the output

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,7 +5,8 @@ word_counts = {}
 for word in words:
     word = word.lower()
     word_counts[word] = word_counts.get(word, 0) + 1
+max_word_length = max(len(word) for word in word_counts)
 
 print("Word Counts:")
 for word, count in sorted(word_counts.items()):
-    print(f"{word} : {count}")
+    print(f"{word:{max_word_length}} : {count}")
