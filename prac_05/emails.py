95bed83e0ddea840cffa92bc990951754a5e919b
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 01:47:00 +0800

    corrected output

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,17 @@ def get_name(email):
     name = name.title()
 
     return name
+
+user_data = {}
+email = input("Email: ")
+while email != "":
+    name = get_name(email)
+    confirm_name = input(f"Is your name {name}? (Y/n) ").lower()
+    if confirm_name not in ('', 'y'):
+        name = input("Name: ")
+    user_data[email] = name
+    email = input("Email: ")
+
+print("\ndata:")
+for email, name in user_data.items():
+    print(f"{name} ({email})")
