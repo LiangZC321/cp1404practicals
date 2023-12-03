3f4c95e6ffa77c0e69fba6b2bb574d9482e28d54
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 02:28:24 +0800

    beta

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -17,3 +17,9 @@ with open(filename, "r", encoding="utf-8-sig") as in_file:
 
 print("Wimbledon Champions:")
 
+for champion, wins in sorted(champion_wins.items()):
+    print(f"{champion} {wins}")
+
+sorted_countries = sorted(countries_set)
+print("\nThese countries have won Wimbledon:")
+print(", ".join(sorted_countries))
