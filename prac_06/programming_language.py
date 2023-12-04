51dbc3ee337b9ef175d0dfe9ce3427c24dd44ec5
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 10:32:58 +0800

    import class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+#10:31 start(doing these at home before class since medical issue)
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
