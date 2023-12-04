e9be8412ee585c9ed49ec0db87a62c711b8d4319
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2023-12-04 11:31:37 +0800

    fixed

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -15,35 +15,3 @@ class Guitar:
         return self.get_age(current_year) >= 50
 
 
-def get_user_guitars():
-    guitars = []
-    guitar_number = 1
-
-    name = input("Name: ")
-    while name != "":
-
-        year = int(input("Year: "))
-        cost = float(input("Cost: $"))
-
-        guitar = Guitar(name, year, cost)
-        guitars.append(guitar)
-
-        print(f"{guitar} added.")
-        guitar_number += 1
-        name = input("Name: ")
-
-    return guitars
-
-
-def display_user_guitars(guitars):
-    print("\nThese are my guitars:")
-
-    for i, guitar in enumerate(guitars, 1):
-        vintage_status = "(vintage)" if guitar.is_vintage(2022) else ""
-        print(f"Guitar {i}: {guitar}, worth ${guitar.cost:.2f} {vintage_status}")
-
-
-
-print("My guitars!")
-user_guitars = get_user_guitars()
-display_user_guitars(user_guitars)
