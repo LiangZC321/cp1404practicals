2aebf33f78b1d991c5afc690d432cba1985ab112
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-01-10 12:30:09 +0800

    the loop for checking

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,3 +6,30 @@ def main():
     total_bill = 0.0
 
     print("Let's drive!")
+    while True:
+        print("q)uit, c)hoose taxi, d)rive")
+        choice = input(">>> ").lower()
+        if choice == 'q':
+            break
+        elif choice == 'c':
+            list_taxis(taxis)
+            taxi_choice = get_valid_input("Choose taxi: ", len(taxis))
+            if taxi_choice is not None:
+                current_taxi = taxis[taxi_choice]
+        elif choice == 'd':
+            if current_taxi:
+                distance = float(input("Drive how far? "))
+                current_taxi.drive(distance)
+                trip_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${trip_cost:.2f}")
+                total_bill += trip_cost
+                current_taxi.start_fare()
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${total_bill:.2f}")
+
+    print(f"Total trip cost: ${total_bill:.2f}")
+    print("Taxis are now:")
+    list_taxis(taxis)
