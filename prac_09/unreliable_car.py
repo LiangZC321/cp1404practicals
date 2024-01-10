30551361a1c403b703ca814720571a3ac5213455
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-01-10 11:29:46 +0800

    drive part

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -8,3 +8,10 @@ class UnreliableCar(Car):
         super().__init__(name, fuel)
         self.reliability = reliability
 
+    def drive(self, distance):
+
+        if random.uniform(0, 100) < self.reliability:
+
+            return super().drive(distance)
+        else:
+            return 0
