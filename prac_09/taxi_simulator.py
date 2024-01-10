f5b361773f372b06f55fbd62582256bc67a3a2bd
LiangZC321 <zhicheng.liang@my.jcu.edu.au>
2024-01-10 12:26:14 +0800

    setting up the statement

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,2 +1,8 @@
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
+def main():
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    total_bill = 0.0
+
+    print("Let's drive!")
